c05205e2d6a5d0685831ec2868a034cfea7c4386
KrostaA <andy.krosta@partners.basf.com>
2016-01-22 16:12:33 +0100

    Team Array Bug

--- src/de/plaz3bo/files/Reader.java
+++ src/de/plaz3bo/files/Reader.java
@@ -9,6 +9,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,16 +18,50 @@ import java.io.IOException;
  */
 public class Reader {
 
-    public Reader(String path) throws FileNotFoundException, IOException {
-        FileReader fileReader = new FileReader(path);
-        BufferedReader br = new BufferedReader(fileReader);
-        String s = br.readLine();
-        while
-            (s != null)
-        {
-            System.out.println(s);
-            s = br.readLine();
+    private String teams[];
+    private FileReader fileReader;
+    private final String teamsPfad;
+    BufferedReader br;
+
+    public Reader(String path) {
+        teamsPfad = path;
+        //System.out.println(teams[0]);
+
+    }
+
+    public void fillTeams() throws IOException {
+        try {
+            fileReader = new FileReader(teamsPfad);
+        } catch (FileNotFoundException ex) {
+            System.err.println("Fehler bei 'fillTeams' in der Klasse Reader: 'File not Found exception");
+            return;
         }
+        br = new BufferedReader(fileReader);
+        //System.out.println(br.readLine());
+int i = 0;
+        teams = new String[(int) br.lines().count()];
+        String s = br.readLine();
+        while(s != null){
+            teams[i] = s;
+            System.out.println(br.readLine());
+             s = br.readLine();
+            i++;
+        }
+        
+
+                
+                System.out.println(s);
+
+//            } catch (IOException ex) {
+//                System.err.println("Fehler beim befüllen des Teamarrays in der Klasse Reader");
+//            }
+        }
+
+    
+
+    @Override
+    public String toString() {
+        return teams[1];
     }
 
 }
