80160558027ffed287db9c8aa95ff7817c443cc6
KrostaA <andy.krosta@partners.basf.com>
2016-01-22 15:44:26 +0100

    Add Teams

--- src/de/plaz3bo/main/Main.java
+++ src/de/plaz3bo/main/Main.java
@@ -18,7 +18,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Reader r = new Reader("C://Users//KrostaA//Documents//NetBeansProjects//Printer Tool//src//de//plaz3bo//files//TeamConfig//Teams.in");
+        Reader r = new Reader("C://Users//KrostaA//Documents//NetBeansProjects//Printer Tool//src//de//plaz3bo//files//TeamConfig//Teams.ini");
         r.fillTeams();
 
     }
